runs, covariance of gaussian samples correct, transition rates don't agree with matlab code

python/CompareSamplers.py
@@ -1,4 +1,4 @@
-from qNHMC import qNHMC
+from LAHMC import LAHMC
 import matplotlib.pyplot as plt
 import distributions
 import numpy as np
@@ -60,14 +60,16 @@ def save_results(history, filename):
     """
     np.savez(filename, history=history)
 
-def run_all(base_filename, num_steps=1000, nbatch=100):
-    np.random.seed(0) # make experiments repeatable
+def run_all(base_filename, num_steps=1000, nbatch=2000):
+    #np.random.seed(0) # make experiments repeatable
 
     filename = "%s_%s.npz"%(base_filename, datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
     history = dict()
 
+    #np.random.seed(0) # make experiments repeatable
     distribution_list = [distributions.Gaussian(ndims=2, nbatch=nbatch, log_conditioning=6), ]
     sampler_list = ['LAHMC', 'HMC',]
+    #sampler_list = ['HMC',]
 
     for distribution in distribution_list:
         cw = counting_wrapper(distribution.E, distribution.dEdX)
@@ -75,17 +77,25 @@ def run_all(base_filename, num_steps=1000, nbatch=100):
         history[dist_name] = dict()
         for sampler_name in sampler_list:
             if sampler_name == 'LAHMC':
-                sampler = sampler_class(distribution.Xinit, cw.E, cw.dEdX)
+                sampler = LAHMC(distribution.Xinit, cw.E, cw.dEdX, epsilon=1., beta=0.1)
             elif sampler_name == 'HMC':
+                # 1 look-ahead step corresponds to standard HMC
+                sampler = LAHMC(distribution.Xinit, cw.E, cw.dEdX, epsilon=1., beta=0.1, num_look_ahead_steps=1)
             else:
                 raise Exception("unknown sampler %s"%(sampler_name))
-            np.random.seed(0) # make experiments repeatable
+            # np.random.seed(0) # make experiments repeatable
             history[dist_name][sampler_name] = []
             for ii in range(num_steps):
-                X = sampler.sample(num_steps = 10)
+                X = sampler.sample(num_steps = 25)
                 history[dist_name][sampler_name].append({'X':X, 'E_count':cw.E_count, 'dEdX_count':cw.dEdX_count})
             # save the current state of the history
             save_results(history, filename)
+
+            # DEBUG
+            print np.dot(X, X.T)/nbatch
+            print np.dot(distribution.Xinit, distribution.Xinit.T)/nbatch
+        1./0
+
     return filename
 
 if __name__ == '__main__':

python/LAHMC.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import defaultdict
 
 class HMC_state(object):
     """ Holds all the state variables for qNHMC. """
@@ -7,36 +8,30 @@ class HMC_state(object):
         self.parent = parent
         self.X = X
         self.V = V
+        nbatch = X.shape[1]
+        self.active_idx = np.arange(nbatch)
         if V is None:
             N = self.X.shape[0]
-            nbatch = self.X.shape[1]
             self.V = np.random.randn(N, nbatch)
         self.EX = EX
         if EX is None:
+            self.EX = np.zeros((1,nbatch))
             self.update_EX()
         self.EV = EV
         if EV is None:
-            self.EV = self.update_EV()
+            self.EV = np.zeros((1,nbatch))
+            self.update_EV()
         self.dEdX = dEdX
         if dEdX is None:
+            self.dEdX = np.zeros(X.shape)
             self.update_dEdX()
-        self.active_idx = False
 
-    def update_EX():
-        if self.active_idx:
-            self.EX[:,self.active_idx] = parent.E(self.X[:,self.active_idx])
-        else:
-            self.EX = parent.E(self.X)
-    def update_EV():
-        if self.active_idx:
-            self.EV[:,self.active_idx] = np.sum(self.V[:,self.active_idx]**2, axis=0).reshape((1,-1))/2.
-        else:
-            self.EV = np.sum(self.V**2, axis=0).reshape((1,-1))/2.
-    def update_dEdX():
-        if self.active_idx:
-            self.dEdX[:,self.active_idx] = parent.dEdX(self.X[:,self.active_idx])
-        else:
-            self.dEdX = parent.dEdX(self.X)
+    def update_EX(self):
+        self.EX[:,self.active_idx] = self.parent.E(self.X[:,self.active_idx])
+    def update_EV(self):
+        self.EV[:,self.active_idx] = np.sum(self.V[:,self.active_idx]**2, axis=0).reshape((1,-1))/2.
+    def update_dEdX(self):
+        self.dEdX[:,self.active_idx] = self.parent.dEdX(self.X[:,self.active_idx])
 
     def copy(self):
         Z = HMC_state(self.X.copy(), self.parent, V=self.V.copy(), EX=self.EX.copy(), EV=self.EV.copy(), dEdX=self.dEdX.copy())
@@ -58,9 +53,10 @@ class LAHMC(object):
     quasiNewton Hamiltonian Monte Carlo sampler
     """
 
-    def __init__(self, Xinit, E, dEdX, num_leapfrog_steps=10, num_look_ahead_steps=5, epsilon=0.1, alpha=0.2, beta=None, display=10):
+    def __init__(self, Xinit, E, dEdX, num_leapfrog_steps=10, num_look_ahead_steps=4, epsilon=0.1, alpha=0.2, beta=None, display=1):
 
         self.M = num_leapfrog_steps
+        self.K = num_look_ahead_steps
         self.N = Xinit.shape[0]
         self.nbatch = Xinit.shape[1]
         self.display = display
@@ -94,26 +90,16 @@ class LAHMC(object):
 
     def H(self, state):
         """ The Hamiltonian for the full system """
-        if state.active_idx:
-            idx = state.active_idx
-            H = state.EX[:,idx] + state.EV[:,idx]
-        else:
-            H = state.EX + state.EV
+        H = state.EX + state.EV
         return H
 
     def leapfrog(self, state):
         """ A single leapfrog step for X and V """
-        if state.active_idx:
-            idx = state.active_idx
-            state.V[:,idx] += -self.epsilon/2. * state.dEdX[:,idx]
-            state.X[:,idx] += self.epsilon * state.V[:,idx]
-            state.update_dEdX()
-            state.V[:,idx] += -self.epsilon/2. * state.dEdX[:,idx]
-        else:
-            state.V += -self.epsilon/2. * state.dEdX
-            state.X += self.epsilon * state.V
-            state.update_dEdX()
-            state.V += -self.epsilon/2. * state.dEdX
+        idx = state.active_idx
+        state.V[:,idx] += -self.epsilon/2. * state.dEdX[:,idx]
+        state.X[:,idx] += self.epsilon * state.V[:,idx]
+        state.update_dEdX()
+        state.V[:,idx] += -self.epsilon/2. * state.dEdX[:,idx]
         return state
 
     def L(self, state):
@@ -124,11 +110,6 @@ class LAHMC(object):
         state.update_EX()
         return state
 
-    def F(self, state):
-        """ Flip the momentum """
-        state.V = -state.V
-        return state
-
     def leap_prob(self, Z1, Z2):
         """
         Metropolis-Hastings Probability of transitioning from state Z1 to
@@ -137,30 +118,33 @@ class LAHMC(object):
         EZ1 = self.H(Z1)
         EZ2 = self.H(Z2)
         Ediff = EZ1 - EZ2
-        p_acc = np.ones(1, Ediff.shape[1])
+        p_acc = np.ones((1, Ediff.shape[1]))
         p_acc[Ediff<0] = np.exp(Ediff[Ediff<0])
         return p_acc
 
-    def leap_prob_recurse(self, Z_chain, C):
+    def leap_prob_recurse(self, Z_chain, C, active_idx):
         if np.isfinite(C[0,-1,0]):
             # we've already visited this leaf
-            cumu = C[0,-1,:]
+            cumu = C[0,-1,:].reshape((1,-1))
             return cumu, C
 
         if len(Z_chain) == 2:
             # the two states are one apart
             p_acc = self.leap_prob(Z_chain[0], Z_chain[1])
+            p_acc = p_acc[:,active_idx]
             C[0,-1,:] = p_acc
             return p_acc, C
 
-        cum_forward, Cl = self.leap_prob_recurse(Z_chain[:-1], C[:-1,:-1,:])
+        cum_forward, Cl = self.leap_prob_recurse(Z_chain[:-1], C[:-1,:-1,:], active_idx)
         C[:-1,:-1,:] = Cl
-        cum_reverse, Cl = self.leap_prob_recurse(Z_chain[:0:-1], C[:0:-1,:0:-1,:])
+        cum_reverse, Cl = self.leap_prob_recurse(Z_chain[:0:-1], C[:0:-1,:0:-1,:], active_idx)
         C[:0:-1,:0:-1,:] = Cl
 
         H0 = self.H(Z_chain[0])
         H1 = self.H(Z_chain[1])
-        start_state_ratio = np.exp(H1 - H2)
+        Ediff = H0 - H1
+        Ediff = Ediff[:,active_idx]
+        start_state_ratio = np.exp(Ediff)
         prob = np.min(np.vstack((1. - cum_forward, start_state_ratio*(1. - cum_reverse))), 0)
         cumu = cum_forward + prob
         C[0,-1,:] = cumu
@@ -170,39 +154,41 @@ class LAHMC(object):
         """ Perform a single sampling step. """
 
         # first do the HMC part of the step
-        Z_chain = (self.state.copy(),)
+        Z_chain = [self.state.copy(),]
         # use the same random number for comparison for the entire chain
-        rand_comparison = np.random.rand(1, self.nbatch)
+        rand_comparison = np.random.rand(1, self.nbatch).ravel()
         # the current cumulative probability of acceptance
-        p_cum = np.zeros(1, self.nbatch)
+        p_cum = np.zeros((1, self.nbatch))
         # the cumulative probability matrix, so we only need to visit each leaf once when recursing
-        C = np.ones((self.K+1, self.K+1, nbatch))*np.nan
+        C = np.ones((self.K+1, self.K+1, self.nbatch))*np.nan
         # the current set of indices for samples that have not yet been accepted for a transition
         active_idx = np.arange(self.nbatch, dtype=int)
         for kk in range(self.K):
             Z_chain.append(self.L(Z_chain[-1].copy()))
             # recursively calculate the cumulative probability of doing this many leaps
-            p_cum, Cl = self.leap_prob_recurse(Z_chain, C[:kk, :kk, active_idx])
+            p_cum, Cl = self.leap_prob_recurse(Z_chain, C[:kk+1, :kk+1, active_idx], active_idx)
+            C[:kk+1, :kk+1, active_idx] = Cl
             # find all the samples that did this number of leaps, and update self.state with them
-            accepted_idx = active_idx[p_cum >= rand_comparison[active_idx]]
+            accepted_idx = active_idx[p_cum.ravel() >= rand_comparison[active_idx]]
             self.counter['L%d'%(kk+1)] += len(accepted_idx)
             self.state.update(accepted_idx, Z_chain[-1])
             # update the set of active indices, so we don't do simulate trajectories for samples that are already assigned to a state
-            active_idx = np.flatnonzero(p_cum < rand_comparison)
+            active_idx = active_idx[p_cum.ravel() < rand_comparison[active_idx]]
+            if len(active_idx) == 0:
+                break
             Z_chain[-1].active_idx = active_idx
-        # flip the momenutm for any samples that were unable to 
+        # flip the momenutm for any samples that were unable to place elsewhere
         self.state.V[:,active_idx] = -self.state.V[:,active_idx]
         self.counter['F'] += len(active_idx)
-        # update the current state
-        self.state = SZ
+
         if self.display > 1:
             print "Transition counts ",
-            for k in self.counter.keys():
+            for k in sorted(self.counter.keys()):
                 print "%s:%d"%(k, self.counter[k]),
-            print
 
         # corrupt the momentum
         self.state.V = self.state.V*np.sqrt(1.-self.beta) + np.random.randn(self.N,self.nbatch)*np.sqrt(self.beta)
+        self.state.update_EV()
 
     def sample(self, num_steps=100):
         """
@@ -210,9 +196,19 @@ class LAHMC(object):
         This is the function to call from external code.
         """
         for si in range(num_steps):
-            if self.display > 0:
+            if self.display > 1:
                 print "sampling step %d / %d,"%(si+1, num_steps),
             self.sampling_iteration()
-            if self.display > 0:
+            if self.display > 1:
                 print 
+
+        if self.display > 0:
+            tot = 0
+            for k in sorted(self.counter.keys()):
+                tot += self.counter[k]
+            print "Transition fraction ",
+            for k in sorted(self.counter.keys()):
+                print "%s:%g"%(k, self.counter[k]/float(tot)),
+            print
+
         return self.state.X